e90828ce0d346f5ce7a8d453b38d9cf89f410b7f
“Victoria <victoriialarsen@gmail.com>
2025-01-17 16:24:37 +0200

    All the rules fot checking the password added

--- password.js
+++ password.js
@@ -1,1 +1,10 @@
+function checkPassword (yourPassword) {
+    if (yourPassword.length >= 8 && /\d/.test(yourPassword) && yourPassword.toUpperCase() != yourPassword 
+    && yourPassword.toLowerCase() != yourPassword) { 
+        return 'Strong'
+    } else {
+        return 'Weak'
+    }
+}
 
+console.log(checkPassword('1234qwertU'));
